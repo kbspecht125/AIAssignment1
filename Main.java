9f911b31c3a591f5069e659a925795cb74ad567f
tdlaovoravit <tdlaovoravit@wpi.edu>
2016-09-08 00:15:15 -0400

    Changed input/ arrayList
    Changed input to be able to take in varying number of operations, changed to an arrayList instead of an array.
    (this is coded for user input and not .txt file

--- Main.java
+++ Main.java
@@ -2,17 +2,40 @@ package main;
 
 import java.util.Scanner;
 import java.util.LinkedList;
+import java.util.ArrayList;
 public class Main {
+  int count=0;
+  int start,goal;
+  double endTime;
 public static void main(String [] args){
-Scanner user_input = new Scanner(System.in);
-String start  = /*user_input.nextLine();*/ "10";
-String goal  = /*user_input.nextLine();*/"89";
-String op1  = /*user_input.nextLine();*/"- 5";
-String op2  = /*user_input.nextLine();*/"+ 4";
-String op3  = /*user_input.nextLine();*/"- 3";
-String op4  = /*user_input.nextLine();*/"+ 0";
-String op5  = /*user_input.nextLine();*/"^ 2";
-String[] operations = new String[]{op1,op2,op3,op4,op5};
+  ArrayList<String> operations = new ArrayList<String>();
+  Scanner user_input = new Scanner(System.in);
+  String line= user_input.nextLine();
+  while(line != "\n"){
+    if(count==0){
+      start = Integer.parseInt(line);
+    }
+    else if(count==1){
+      goal = Integer.parseInt(line);
+    }
+    else if(count==2){
+      endTime = Double.parseDouble(line);
+    }
+    else{
+      operations.add(line);
+    }
+    count++;
+    line = user_input.nextLine();
+  }
+  
+//String start  = /*user_input.nextLine();*/ "10";
+//String goal  = /*user_input.nextLine();*/"89";
+//String op1  = /*user_input.nextLine();*/"- 5";
+//String op2  = /*user_input.nextLine();*/"+ 4";
+//String op3  = /*user_input.nextLine();*/"- 3";
+//String op4  = /*user_input.nextLine();*/"+ 0";
+//String op5  = /*user_input.nextLine();*/"^ 2";
+//String[] operations = new String[]{op1,op2,op3,op4,op5};
 Node n = new Node("", operations, new LinkedList<String>(), Integer.parseInt(goal), 1);
 n.data = Integer.parseInt(start);
 n.greedyAlgorithm();
