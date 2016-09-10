ccb6b92080b74371c01acdaebd975604a85ef448
tdlaovoravit <tdlaovoravit@wpi.edu>
2016-09-09 21:02:23 -0400

    Mike's changes on Node

--- Node.java
+++ Node.java
@@ -4,18 +4,67 @@ import java.util.LinkedList;
 public class Node {
 int data;
 int goal;
+int yields;//NEWWWWWWWWWWWWWWWWWWW
+Node parent;
+boolean isexpanded;
 int nodesexpanded;
 String operation;
 String[] operations;
 LinkedList<String> history;
 LinkedList<Node> nodeops;
 public Node(String op, String[] ops, LinkedList<String> his, int g, int nodesex){
-op = operation;
-nodesexpanded = nodesex;
-goal = g;
-operations = ops;
-nodeops = new LinkedList<Node>();
-history = his;
+this.yields = -1;
+this.isexpanded = false;
+this.operation = op;
+this.nodesexpanded = nodesex;
+this.goal = g;
+this.operations = ops;
+this.nodeops = new LinkedList<Node>();
+this.history = his;
+}
+public LinkedList<String> pulse(){
+int d = 0;
+while(d < 4){
+LinkedList<String> ret = pulseRoot(d);
+if(ret != null){
+return ret;
+}else{
+d++;
+}
+}
+return null;
+}
+public LinkedList<String> parentOps(){
+LinkedList<String> lst = new LinkedList<String>();
+lst.addFirst(operation);
+return parentOpsAcc(lst);
+}
+public LinkedList<String> parentOpsAcc(LinkedList<String> lst){
+if(parent != null && parent.operation != ""){
+lst.addFirst(parent.operation);
+return parent.parentOpsAcc(lst);
+}else{
+return lst;
+}
+}
+public LinkedList<String> pulseRoot(int dep){
+if(dep == 0){
+if(data == goal){
+return parentOps();
+}else{
+return null;
+}
+}
+if(nodeops.size() == 0){ expand();}
+for(Node n : nodeops){
+int a = dep - 1;
+System.out.println(data + " " + n.operation + "=" + n.operate(data));
+LinkedList<String> ret = n.pulseRoot(dep - 1);
+if(ret != null){
+return ret;
+}
+}
+return null;
 }
 public void printeverything(){
 System.out.println("Data - " + data);
@@ -35,23 +84,48 @@ System.out.println("nodeops - " + n.data + " " + n.operation);
 public void greedyAlgorithm(){
 expand();
 //printeverything();
-Node next = bestNode();
-if(next == null){
+if(data - goal == 0){
 int diff = Math.abs(goal - data);
 System.out.println("Error: " + diff);
 System.out.println("Number of steps required: " + history.size());
 System.out.println("Nodes expanded: " + nodesexpanded);
 }else{
+Node next = bestNode();
 System.out.println(data + " " + next.operation + " = " + next.data);
 next.greedyAlgorithm();
 }
 }
-public Node bestNode(){ //finds the node with the shortest distance from the goal, null if all distances are shorter than current one
-int bestdistancefromgoal = Math.abs(data - goal);
-Node best = null;
+public int newGreedy(){
+if(isexpanded){
+this.yields = bestNode().newGreedy();
+System.out.println(data + " y=" + yields);
+return yields;
+}else{
+expand();
+System.out.println(data + " " + bestNode().operation + " = " + bestNode().data);
+this.yields = Math.abs(bestNode().data - goal);
+System.out.println(data + " y=" + yields);
+return yields;
+}
+}
+public boolean grandNewGreedy(){
+int i = 1;
+while(i != 0){
+i = newGreedy();
+}
+return true;
+}
+public Node bestNode(){ //finds the node with the shortest distance from the goal
+Node best = nodeops.getFirst();
+int bestdistancefromgoal = Math.abs(best.operate(data) - goal);
 for(Node n : nodeops){
-int nodesdistancefromgoal = Math.abs(n.operate(data) - goal);
-if(nodesdistancefromgoal < bestdistancefromgoal){
+int nodesdistancefromgoal;
+if(n.yields == -1){
+nodesdistancefromgoal = Math.abs(n.operate(data) - goal);
+}else{
+nodesdistancefromgoal = n.yields;
+}
+if(nodesdistancefromgoal < bestdistancefromgoal /*&& n.isexpanded == false*/){
 best = n;
 bestdistancefromgoal = nodesdistancefromgoal;
 }
@@ -59,20 +133,20 @@ bestdistancefromgoal = nodesdistancefromgoal;
 return best;
 }
 public void expand(){ //populates node with children nodes with data and operation values
+isexpanded = true;
 if(operation != null){
 history.addLast(operation);
 }
 for(int i = 0; i < 5; i++){
-LinkedList<String> remember = history;
-Node n = new Node(operations[i], operations, history, this.goal, nodesexpanded + 1);
-n.operation = operations[i];//the operation isn't assigned via constructor, not sure why
+Node n = new Node(operations[i], operations, this.history, this.goal, nodesexpanded + 1);
+n.parent = this;
 n.data = n.operate(data);
-this.history = remember;
 nodeops.addLast(n);
 }
 }
 public int operate(int parentnum){ //parent 'operation' = return value
-int secondnumber = Integer.parseInt(operation.substring(operation.length() - 1));
+String num = operation.replaceAll(" ", "");
+int secondnumber = Integer.parseInt(num.substring(1, num.length()));
 String startop = operation.substring(0, 1);
 if(startop.equals("+")){
 return parentnum + secondnumber;
